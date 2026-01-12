41bf4db0945b89df305ea3a019555eeacd6c4fbc
ceydaa63 <uygceyda@gmail.com>
2026-01-12 19:21:34 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ceydauygun2420161055
+//
+//  Created by ceyda on 12.01.2026.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
